d987e4245444e688c1065b7a2b9ac09718a62d83
win4er <104188509+win4er@users.noreply.github.com>
2023-10-08 22:52:07 +0300

    Update count_words.h

--- count_words.h
+++ count_words.h
@@ -16,14 +16,15 @@ void Sort(std::map<std::string, int>& M);
 class CountWords {
     private:
     //здесь можно объявлять любые необходимые переменные
-	std::string file;
-	std::map<std::string, int> countWords;
+        std::string file;
+        std::map<std::string, int> countWords;
     public:
-	CountWords(std::string file);
-	~CountWords();
-	int getCountWord(const std::string& word) const;
-	int addWord(const std::string& word); //возвращает число слов, встретившихся в тексте
-	const std::list<std::pair<std::string, int>>& getMaxCountWord() const; // возвращает список слов, которые встретились чаще других не более 10 слов
-	const std::list<std::pair<std::string, int>>& getMinCountWord() const; // возвращает список слов, которые встретились реже других не более 10 слов
+        CountWords(std::string file);
+        ~CountWords();
+        int getCountWord(const std::string& word) const;
+        int addWord(const std::string& word); //возвращает число слов, встретившихся в тексте
+        std::list<std::pair<std::string, int>> getMaxCountWord() const; // возвращает список слов, которые встретились чаще других не более 10 слов
+        std::list<std::pair<std::string, int>> getMinCountWord() const; // возвращает список слов, которые встретились реже других не более 10 слов
 };
 #endif
+
