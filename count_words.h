03b02b82bd0c2560b980fbaad0d05e2935eb0387
win4er <104188509+win4er@users.noreply.github.com>
2023-10-13 14:53:01 +0300

    Update count_words.h

--- count_words.h
+++ count_words.h
@@ -2,30 +2,21 @@
 #define COUNT_WORDS_H
 
 #include <iostream>
-#include <map>
-#include <list>
-#include <vector>
 #include <string>
-#include <utility>
-#include <fstream>
-#include <bits/stdc++.h>
-
-bool cmp(std::pair<std::string, int>& a, std::pair<std::string, int>& b);
-void Sort(std::map<std::string, int>& M);
+#include <list>
+#include <map>
+#include <climits>
+#include <algorithm>
 
 class CountWords {
     private:
-    //здесь можно объявлять любые необходимые переменные
-        std::string file;
         std::map<std::string, int> countWords;
     public:
         CountWords();
-        CountWords(std::string file);
         ~CountWords();
         int getCountWord(const std::string& word) const;
-        int addWord(const std::string& word); //возвращает число слов, встретившихся в тексте
-        std::list<std::pair<std::string, int>> getMaxCountWord() const; // возвращает список слов, которые встретились чаще других не более 10 слов
-        std::list<std::pair<std::string, int>> getMinCountWord() const; // возвращает список слов, которые встретились реже других не более 10 слов
+        int addWord(const std::string& word);
+        std::list<std::pair<std::string, int>> getMaxCountWord() const;
+        std::list<std::pair<std::string, int>> getMinCountWord() const;
 };
 #endif
-
