3a5ade45140157bc373583cbeb507b1071ca050a
win4er <104188509+win4er@users.noreply.github.com>
2023-10-20 09:00:48 +0300

    Update count_words.h

--- count_words.h
+++ count_words.h
@@ -1,16 +1,18 @@
 #ifndef COUNT_WORDS_H
 #define COUNT_WORDS_H
 
-#include <iostream>
 #include <string>
 #include <list>
 #include <map>
+#include <vector>
+#include <tuple>
 #include <climits>
 #include <algorithm>
 
 class CountWords {
     private:
         std::map<std::string, int> countWords;
+        std::map<std::pair<std::string, std::string>, int> countWordsPairs;
     public:
         CountWords();
         ~CountWords();
@@ -18,5 +20,8 @@ class CountWords {
         int addWord(const std::string& word);
         std::list<std::pair<std::string, int>> getMaxCountWord() const;
         std::list<std::pair<std::string, int>> getMinCountWord() const;
+        int insertWordsPair(const std::pair<std::string, std::string>& pair_word);
+        std::tuple<std::string, std::string, int> getWordPair(const std::pair<std::string, std::string>& pair_word);
 };
 #endif
+
