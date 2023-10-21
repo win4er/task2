39ceeefdee6a552e19f8ff3800d379d929da3aa5
win4er <104188509+win4er@users.noreply.github.com>
2023-10-21 17:56:23 +0300

    Create task2.h

---
+++ task2.h
@@ -0,0 +1,20 @@
+#ifndef TASK2_H
+#define TASK2_H
+
+class Rational {
+	private:
+		int numerator_;
+		int denominator_;
+		int gcd(int a, int b);
+	public:
+		Rational();
+		Rational(int numerator);
+		Rational(int numerator, int denominator);
+
+		static Rational Add(const Rational& a, const Rational& b);
+
+		int numerator() const;
+		int denominator() const;
+};
+
+#endif
