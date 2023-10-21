1b126d6b9eb6de32f2c8679970da234df121f925
win4er <104188509+win4er@users.noreply.github.com>
2023-10-21 18:08:14 +0300

    Create task2.cpp

---
+++ task2.cpp
@@ -0,0 +1,44 @@
+#include "task2.h"
+
+int Rational::gcd(int a, int b){
+    if (a > b){
+      a = a + b;
+      b = a - b;
+      a = a - b;
+    }
+    if ((a == (b - a)) or (a == b)) {
+      return a;
+    }
+    return gcd(a, b - a);
+}
+
+Rational::Rational(){
+    numerator_ = 0;
+	  denominator_ = 1;
+}
+
+Rational::Rational(int numerator){
+    numerator_ = numerator;
+    denominator_ = 1;
+}
+
+Rational::Rational(int numerator, int denominator){
+    numerator_ = numerator;
+    denominator_ = denominator;
+}
+
+Rational Rational::Add(const Rational& a, const Rational& b){
+    Rational r;
+    int GCD = r.gcd(a.denominator(), b.denominator());
+    int numer = a.numerator() * b.denominator() / GCD + b.numerator() * a.denominator() / GCD;
+    int denomer = a.denominator() * b.denominator() / GCD;
+    return Rational(numer, denomer);
+}
+
+int Rational::numerator() {
+    return numerator_;
+}
+
+int Rational::denominator() {
+    return denominator_;
+}
