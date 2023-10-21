4c7da57be8e0c27b1ab63852c4548b646824587b
win4er <104188509+win4er@users.noreply.github.com>
2023-10-21 17:58:17 +0300

    Update task1.cpp

--- task1.cpp
+++ task1.cpp
@@ -1,22 +1,22 @@
 #include <string>
 
 class Cat {
-  public:
-		Cat() = delete;
-		Cat(const std::string& name){
-			name_ = name;
-		}
-		std::string GetName() const {
-			return name_;
-		}
-	private:
-		std::string name_;
+    public:
+        Cat() = delete;
+	Cat(const std::string& name){
+	    name_ = name;
+	}
+	std::string GetName() const {
+  	    return name_;
+	}
+private:
+    std::string name_;
 };
 
 class Witch {
-	public:
-		Witch(const std::string& catName) : cat_(catName) {}
-	private:
-		Cat cat_;
+    public:
+        Witch(const std::string& catName) : cat_(catName) {}
+    private:
+	Cat cat_;
 };
 
