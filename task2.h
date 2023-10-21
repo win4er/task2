61766a734e73601b445b0da2bbc20f09921dd47e
win4er <104188509+win4er@users.noreply.github.com>
2023-10-21 17:59:34 +0300

    Update task2.h

--- task2.h
+++ task2.h
@@ -2,19 +2,19 @@
 #define TASK2_H
 
 class Rational {
-	private:
-		int numerator_;
-		int denominator_;
-		int gcd(int a, int b);
-	public:
-		Rational();
-		Rational(int numerator);
-		Rational(int numerator, int denominator);
+    private:
+	int numerator_;
+	int denominator_;
+        int gcd(int a, int b);
+    public:
+	Rational();
+	Rational(int numerator);
+	Rational(int numerator, int denominator);
 
-		static Rational Add(const Rational& a, const Rational& b);
+        static Rational Add(const Rational& a, const Rational& b);
 
-		int numerator() const;
-		int denominator() const;
+        int numerator() const;
+	int denominator() const;
 };
 
 #endif
